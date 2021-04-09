cf88d8e31717a59a94cd08969006d94e08a9eee2
Darkship <82195509+Darkship86@users.noreply.github.com>
2021-04-09 10:43:53 +0545

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,7 @@
+const Darkship = {
+  nickname: "Dan",
+  Discord: "Darkship#7479",
+  JavascriptLover: true,
+  Languages: [Javascript, Python, Regex],
+  Tools: [ Nodejs, Tensorflowjs,  Docker],
+}
