af6be8a7680a5e995a321464443fbb924644d8bf
Darkship <82195509+Darkship86@users.noreply.github.com>
2021-04-09 11:09:45 +0545

    Update and rename README.md to README.js

--- README.js
+++ README.js
@@ -6,4 +6,4 @@ const Darkship = {
   Tools: [ Nodejs, Tensorflowjs,  Docker],
 }
 
-![Github stats](https://github-readme-stats.vercel.app/api?username=Darkship&theme=react&show_icons=true&count_private=true)
+
